4c682a931769eddfb62f8f506da678f52175435d
Multy Pony <meshcheryakovvrn@gmail.com>
2017-09-07 02:39:09 +0300

    updated posNeg.js

--- source/CodingBat/Warmup-1/posNeg.js
+++ source/CodingBat/Warmup-1/posNeg.js
@@ -1,4 +1,3 @@
-//posNeg.js
 /*
   Given 2 int values, return true if one is negative
   and one is positive. Except if the parameter
@@ -12,7 +11,7 @@ console.log(posNeg(-4,-5,false));
 
 function posNeg(a, b, negative) {
   if(negative) return (a < 0 && b < 0);
-  return (a * b) < 0;
+  return (a < 0 && b > 0) || (b < 0 && a > 0);
 }
 
 /* First solution
