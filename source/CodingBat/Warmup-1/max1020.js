b2570f831e7a092de6128a8496e7d25c1cf09222
Multy Pony <meshcheryakovvrn@gmail.com>
2017-09-07 00:25:21 +0300

    mod max1020.js

--- source/CodingBat/Warmup-1/max1020.js
+++ source/CodingBat/Warmup-1/max1020.js
@@ -4,17 +4,21 @@
   that is in the range 10..20 inclusive, or return 0
   if neither is in that range.
 */
-console.log(max1020(11,19));
-console.log(max1020(19,11));
-console.log(max1020(11,9));
-console.log(max1020(9,21));
-console.log(max1020(10,21));
+console.log(max1020(11,19)); //19
+console.log(max1020(19,11)); //19
+console.log(max1020(11,9));  //11
+console.log(max1020(9,21));  //0
+console.log(max1020(10,21)); //10
 
 function max1020(a, b) {
-  var inRangeA = (a >= 10 && a <= 20);
-  var inRangeB = (b >= 10 && b <= 20);
-  if(!inRangeA && !inRangeB) return 0;
-  if(inRangeA && inRangeB) return Math.max(a,b);
-  else if(inRangeA && !inRangeB) return a;
-  return b;
+  // var inRangeA = (a >= 10 && a <= 20);
+  // var inRangeB = (b >= 10 && b <= 20);
+  if(!inRange(a) && !inRange(b)) return 0;
+  if(inRange(a) && inRange(b)) return Math.max(a,b);
+  return (inRange(a) && !inRange(b)) ? a : b;
+  // else if(inRangeA && !inRangeB) return a;
+  // return b;
+}
+function inRange(x) {
+  return (x >= 10 && x <= 20);
 }
