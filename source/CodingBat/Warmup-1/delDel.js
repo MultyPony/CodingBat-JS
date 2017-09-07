37f738b36d135b0a9f83e4e2518b2c08f4859856
Multy Pony <meshcheryakovvrn@gmail.com>
2017-09-07 13:37:31 +0300

    added regEx

--- source/CodingBat/Warmup-1/delDel.js
+++ source/CodingBat/Warmup-1/delDel.js
@@ -1,17 +1,16 @@
-//delDel.js
 /*
   Given a string, if the string "del" appears starting at
   index 1, return a string where that "del" has been
   deleted. Otherwise, return the string unchanged.
 */
-console.log(delDel("adelbc"));
-console.log(delDel("adelHello"));
-console.log(delDel("adedbc"));
-console.log(delDel("abcdel"));
-console.log(delDel("add"));
+console.log(delDel("adelbc"));     //abc
+console.log(delDel("adelHello"));  //aHello
+console.log(delDel("adedbc"));     //adedbc
+console.log(delDel("abcdel"));     //abcdel
+console.log(delDel("add"));        //add
 
 function delDel(str) {
-  return (str.length > 3 &&
-        str.slice(1,4) == "del") ?
-        str[0] + str.slice(4) :  str;
+  var regex = /del/y;
+  regex.lastIndex = 1;
+  return str.replace(regex,'');
 }
